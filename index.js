557bc82087e568d0d81eeb04ec99c9fc68c41367
Yau <DonYau@126.com>
2020-03-03 14:09:11 +0800

    Updates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-var ghpages = require('gh-pages');
+var ghpages = require("gh-pages");
 
-ghpages.publish('_book',{
-    branch:"master",
-    repo:"https://github.com/YolandaQingniu/YolandaQingniu.github.io.git",
-}, function(err) {
-    console.log(err)
-});
+ghpages.publish(
+  "_book",
+  {
+    branch: "gh-pages",
+    repo: "https://github.com/DonYau/DonYauQingniu.github.io.git"
+  },
+  function(err) {
+    console.log(err);
+  }
+);
