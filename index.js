cbe53d9835dcefe521c95dbda2a10721bf49ebec
DonYau <DonYau@126.com>
2020-03-13 09:29:15 +0800

    Updates

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var ghpages = require("gh-pages");
 ghpages.publish(
   "_book",
   {
+<<<<<<< HEAD
     branch: "gh-pages",
     repo: "https://github.com/DonYau/DonYauQingniu.github.io.git"
+=======
+    branch: "master",
+    repo: "https://github.com/YolandaQingniu/YolandaQingniu.github.io.git"
+>>>>>>> dev
   },
   function(err) {
     console.log(err);
